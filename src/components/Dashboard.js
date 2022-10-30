37fc503f608bccfc3435ac0291f7f72bbb35eabd
Hasnain Mohi Ud Din <hasnain.mohiuddin@devsinc.com>
2022-10-30 15:28:02 +0500

    Auth Implemented using Firebase Auth

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import PopularMovies from "./PopularMovies";
+import SearchFormSection from "./SearchFormSection";
+
+const Dashboard = () => {
+    return (
+        <>
+        <SearchFormSection />
+        <PopularMovies />
+        </>
+    )
+}
+
+export default Dashboard;
